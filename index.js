3f906e364141dba6923c5af36ba7e4e61267172e
Sheena Bonilla <sheena.myb@gmail.com>
2022-03-19 22:30:32 -0700

    commit tree 8c02da1e3b14f0c1da62063815e558d7f4cd6271

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+const driver = {name: 'Sam'};
+
+// non destructive
+function updateDriverWithKeyAndValue(obj, key, value) {
+    const updateDriver = { ...obj };
+    updateDriver['address'] = '11 Broadway';
+    return updateDriver
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
+    driver['address'] = '12 Broadway';
+    return driver
+}
+
+// non destructive
+function deleteFromDriverByKey(obj, key) {
+    let newDriver = Object.assign({}, driver);
+    delete newDriver['name'];
+    return newDriver
+}
+
+function destructivelyDeleteFromDriverByKey(obj, key) {
+    delete driver.name;
+    return driver
+}
